bad57154c2896c898529be1b5f71692cedcfe5b9
Quinton Fults <coding.banjo@gmail.com>
2020-05-15 12:00:14 -0700

    code

--- promise.js
+++ promise.js
@@ -1,26 +1,53 @@
-const greeting = (message) => {
+// const greeting = (message) => {
 
-  return new Promise((resolve, reject) => {
-    if (message) {
-      resolve(message)
-    } else {
-      reject(new Error('No message passed'))
-    }
-  })
-}
+//   return new Promise((resolve, reject) => {
+//     if (message) {
+//       resolve(message)
+//     } else {
+//       reject(new Error('No message passed'))
+//     }
+//   })
+// }
 
-greeting('Hello World!')
-  .then(message => {
-    console.log(message)
-  })
-  .catch(err => console.log(err))
+// greeting('Hello World!')
+//   .then(message => {
+//     console.log(message)
+//   })
+//   .catch(err => console.log(err))
 
 
   const Math = equation => {
+    
+    return new Promise((resolve, reject) => {
+      let pieces = equation.split(' ')
+      let a = parseInt(pieces[0])
+      let op = pieces[1]
+      let b = parseInt(pieces[2])
 
+      if (!isNaN(a) && !isNaN(b) && '+-*/'.includes(op)) {
+
+        switch (op) {
+          case '+':
+            resolve(a + b)
+            break
+          case '-':
+            resolve(a - b)
+            break
+          case '*':
+            resolve(a * b)
+            break
+          case '/':
+            resolve(a / b)
+            break
+        }
+      } else {
+        reject(new Error('Invalid Equation'))
+      }
+
+    })
   }
 
-  Math('12 * true')
+  Math('12 / 12')
     .then(result => {
       console.log(result)
     })
